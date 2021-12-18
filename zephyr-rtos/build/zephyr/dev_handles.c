b4948a3bdf11d11a9eb8dde4b5e4bbb3ff78e761
Jakub Szukala <jszukala@internships.antmicro.com>
2021-12-18 15:39:29 +0100

    Project restructure

---
+++ zephyr-rtos/build/zephyr/dev_handles.c
@@ -0,0 +1,31 @@
+#include <device.h>
+#include <toolchain.h>
+
+/* 1 : /soc/clock@40000000:
+ * Direct Dependencies:
+ *   - (/soc)
+ *   - (/soc/interrupt-controller@e000e100)
+ */
+const device_handle_t __aligned(2) __attribute__((__section__(".__device_handles_pass2")))
+__devicehdl_DT_N_S_soc_S_clock_40000000[] = { DEVICE_HANDLE_SEP, DEVICE_HANDLE_SEP, DEVICE_HANDLE_ENDS };
+
+/* 2 : /soc/gpio@50000000:
+ * Direct Dependencies:
+ *   - (/soc)
+ * Supported:
+ *   - (/leds/led_0)
+ *   - (/leds/led_1)
+ *   - (/leds/led_3)
+ */
+const device_handle_t __aligned(2) __attribute__((__section__(".__device_handles_pass2")))
+__devicehdl_DT_N_S_soc_S_gpio_50000000[] = { DEVICE_HANDLE_SEP, DEVICE_HANDLE_SEP, DEVICE_HANDLE_ENDS };
+
+/* 3 : /soc/gpio@50000300:
+ * Direct Dependencies:
+ *   - (/soc)
+ * Supported:
+ *   - (/buttons/button_0)
+ *   - (/leds/led_2)
+ */
+const device_handle_t __aligned(2) __attribute__((__section__(".__device_handles_pass2")))
+__devicehdl_DT_N_S_soc_S_gpio_50000300[] = { DEVICE_HANDLE_SEP, DEVICE_HANDLE_SEP, DEVICE_HANDLE_ENDS };
---
+++ zephyr-rtos/build/zephyr/zephyr.dts.d
@@ -0,0 +1,13 @@
+empty_file.o: /home/js/Projects/zephyr-project/zephyr/misc/empty_file.c \
+ /home/js/Projects/zephyr-project/zephyr/boards/arm/nrf52840dongle_nrf52840/nrf52840dongle_nrf52840.dts \
+ /home/js/Projects/zephyr-project/zephyr/dts/arm/nordic/nrf52840_qiaa.dtsi \
+ /home/js/Projects/zephyr-project/zephyr/dts/common/mem.h \
+ /home/js/Projects/zephyr-project/zephyr/dts/arm/nordic/nrf52840.dtsi \
+ /home/js/Projects/zephyr-project/zephyr/dts/arm/armv7-m.dtsi \
+ /home/js/Projects/zephyr-project/zephyr/dts/common/skeleton.dtsi \
+ /home/js/Projects/zephyr-project/zephyr/dts/arm/nordic/nrf_common.dtsi \
+ /home/js/Projects/zephyr-project/zephyr/include/dt-bindings/i2c/i2c.h \
+ /home/js/Projects/zephyr-project/zephyr/include/dt-bindings/gpio/gpio.h \
+ /home/js/Projects/zephyr-project/zephyr/include/dt-bindings/pinctrl/nrf-pinctrl.h \
+ /home/js/Projects/zephyr-project/zephyr/dts/arm/nordic/override.dtsi \
+ /home/js/Projects/zephyr-project/zephyr/boards/arm/nrf52840dongle_nrf52840/fstab-stock.dts
