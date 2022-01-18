ac507638418a5b62e2d9e68bc5364c3d3a8b6eb1
jszukala <jszukala@internships.antmicro.com>
2022-01-18 19:26:30 +0100

    Add working code from template generator

--- generator.py
+++ generator.py
@@ -6,13 +6,16 @@ loader = FileSystemLoader(os.path.abspath("."))
 env = Environment(loader=loader)
 template = env.get_template("node_init_template.c.j2")
 
-print(template.render(name="Kuba"))
-
 with open("config.json", 'r') as config:
     for line in config:
+        print("Config content:")
         print(line)
-        print(json.loads(line))
-        smth = json.loads(line)
-        print(smth["paths"][0]["addr"])
-        
+        node_config = json.loads(line)
+        out = template.render(
+                addr_t=node_config['addr'],
+                reserved_t=node_config['reserved'],
+                paths_size_t=node_config['paths_size'],
+                paths=node_config['paths']
+                )
+        print(out)
         
