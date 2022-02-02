621e22d441a8c28ca12073e1b9027969fb1166f6
jszukala <jszukala@internships.antmicro.com>
2022-02-02 12:13:38 +0100

    Redesign mobile broadcaster

--- README.md
+++ README.md
@@ -15,17 +15,26 @@ Default Dijkstra graph used in zephyr version is:
 <pre>
 (1)    2   (2)
   
-   1       3
+   1       4
          
 (1)    0   (2)
 </pre>
+
 In brackets are distances between nodes and without brackets are nodes addresses. 
 This information is contained in graph.c file in initialization function.
 This setup allows for simplest case where shortest path must be found. Solution to reach
 node 2 from node 0 is path through 0, 1, 2 with total distance of 2.
 
-File init.resc contains script for Renode platform.
+To build the project, go to zephyr-rtos directory and run build command:
+> cd zephyr-rtos 
+> west build -b nrf52840dk_nrf52840 
 
-Project is still work in progress and I am also just learning Zephyr RTOS.
+Then go back to the root of the project and build mobile broadcaster:
+> cd ../mobile_broadcaster
+> west build -b nrf52840dk_nrf52840 
 
-TODO: Develop this README
+Now You should be ready to run a project. File named init.resc in project root
+directory is for simulation with Renode and should be run with it (use Renode 
+built from sources):
+> ./renode ~/path/to/init.resc
+
---
+++ mobile_broadcaster/src/main.c
@@ -0,0 +1,156 @@
+/* main.c - Application main entry point */
+
+/*
+ * Copyright (c) 2015-2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/types.h>
+#include <stddef.h>
+#include <sys/printk.h>
+#include <sys/util.h>
+
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/hci.h>
+#include <timing/timing.h>
+
+uint8_t mfg_data2[] = {0x00, 0x01, 0x01, 0x01, 0x01 ,0x01, 0x01, 0x01}; 
+const struct bt_data ad2[] = {BT_DATA(0xff, mfg_data2, 8)
+};
+
+
+void main(void)
+{
+    
+    // Timing setup 
+    timing_t start_time, end_time;
+    uint64_t time_in_ns;
+    uint64_t cycles; 
+    
+    // start time measurement
+    timing_init();
+    timing_start();
+    start_time = timing_counter_get();
+
+	int err;
+
+	printk("Starting Broadcaster\n");
+
+    // Initialize BLE 
+    err = bt_enable(NULL);
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+		return;
+	}
+	printk("Bluetooth initialized\n");
+
+    // Receiver address 
+    bt_addr_le_t receiver_addr = {
+        .type = BT_ADDR_LE_RANDOM,
+        .a = { // set it to receiver addr 
+            //      LSB                           MSB
+			.val = {0x22, 0x00, 0x00, 0x00, 0x00 ,0xC0} 
+        }
+    };
+    char r_identity[129];
+    bt_addr_le_to_str(&receiver_addr, r_identity, sizeof(r_identity)); 
+    printk("Sending to node with identity: %s", r_identity); 
+
+    // Advertisement setup 
+    uint32_t ext_adv_aptions = 
+        BT_LE_ADV_OPT_EXT_ADV
+        + BT_LE_ADV_OPT_DIR_MODE_LOW_DUTY;
+        //+ BT_LE_ADV_OPT_NOTIFY_SCAN_REQ;
+        //+ BT_LE_ADV_OPT_CONNECTABLE;
+        //+ BT_LE_ADV_OPT_DIR_MODE_LOW_DUTY;
+
+    struct bt_le_adv_param params = {
+        .id = 0x0,
+        .options = ext_adv_aptions,
+        .peer = &receiver_addr,
+        .interval_min = BT_GAP_ADV_FAST_INT_MIN_1, 
+        .interval_max = BT_GAP_ADV_FAST_INT_MAX_1 
+    };
+
+    // create advertising set
+	struct bt_le_ext_adv *adv;
+	err = bt_le_ext_adv_create(
+            &params, NULL, &adv);
+    if(err){
+        printk("Failed to create advertising set (err %d)\n",
+                err); 
+        return;
+    }
+    
+    // get time in ns as 64b int 
+    end_time = timing_counter_get(); 
+    cycles = timing_cycles_get(&start_time, &end_time);
+    time_in_ns = timing_cycles_to_ns(cycles); 
+    printk("Measured start time: %llu", time_in_ns); 
+
+    // Data setup
+    // First byte is ***mesh address*** of final destination 
+    // Giving time stamp to the packet 
+    uint8_t mfg_data[9]; 
+    memcpy(&mfg_data[1], &time_in_ns, sizeof(time_in_ns));
+    mfg_data[0] = 0x00; 
+    const struct bt_data ad[] = {BT_DATA(0xff, mfg_data, 9)
+    };
+
+    // set advertising set ad and sd 
+    err = bt_le_ext_adv_set_data(adv, ad, ARRAY_SIZE(ad),
+           NULL, 0);
+    if(err){
+        printk("Failed to set adv data (err %d)\n",
+                err); 
+        return;
+    }
+
+    // start advertising
+    err = bt_le_ext_adv_start(adv,
+        BT_LE_EXT_ADV_START_PARAM(0, 3));
+        //BT_LE_EXT_ADV_START_DEFAULT);
+    if (err) {
+        printk("Advertising failed to start (err %d)\n", err);
+        return;
+    }
+
+    // wait some time to finish 	
+    k_msleep(100);
+    
+    // stop it
+    err = bt_le_adv_stop();
+    if (err) {
+        printk("Advertising failed to stop (err %d)\n", err);
+        return;
+    }
+
+    // set new data to adv set 
+    err = bt_le_ext_adv_set_data(adv, ad2, ARRAY_SIZE(ad2),
+            NULL, 0);
+    if(err){
+        printk("Failed to set data for adv (err %d)\n",
+                err); 
+        return;
+    }
+    
+    printk("Second message advertising...\n");
+
+    do{
+        err = bt_le_ext_adv_start(adv,
+            //BT_LE_EXT_ADV_START_DEFAULT); 
+            BT_LE_EXT_ADV_START_PARAM(0, 15));
+    }while(err);
+    
+    if(err){
+        printk("Failed to start adv (err %d)\n",
+                err); 
+        return;
+    }
+	
+	do {
+		k_msleep(100);
+
+	} while (1);
+}
