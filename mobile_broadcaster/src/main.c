9c810b3a1c25e59dee83ad979769104075153a8d
jszukala <jszukala@internships.antmicro.com>
2022-02-11 11:10:49 +0100

    Cleanup mobile broadcaster app from clutter

--- mobile_broadcaster/src/main.c
+++ mobile_broadcaster/src/main.c
@@ -15,24 +15,13 @@
 #include <bluetooth/hci.h>
 #include <timing/timing.h>
 
-uint8_t mfg_data2[] = {0x01, 0x01, 0x01, 0x01, 0x01 ,0x01, 0x01, 0x01}; 
-const struct bt_data ad2[] = {BT_DATA(0xff, mfg_data2, 8)
+uint8_t mfg_data[] = {0x01, 0x01, 0x01, 0x01, 0x01 ,0x01, 0x01, 0x01}; 
+const struct bt_data ad[] = {BT_DATA(0xff, mfg_data, 8)
 };
 
 
 void main(void)
 {
-    /* 
-    // Timing setup 
-    timing_t start_time, end_time;
-    uint64_t time_in_ns;
-    uint64_t cycles; 
-    
-    // start time measurement
-    timing_init();
-    timing_start();
-    start_time = timing_counter_get();
-    */
 	int err;
 
 	printk("Starting Broadcaster\n");
@@ -61,16 +50,13 @@ void main(void)
     // Advertisement setup 
     uint32_t ext_adv_aptions = 
         BT_LE_ADV_OPT_EXT_ADV
-        + BT_LE_ADV_OPT_DIR_MODE_LOW_DUTY;  // TODO:remove that ??
-        //+ BT_LE_ADV_OPT_NOTIFY_SCAN_REQ;
-        //+ BT_LE_ADV_OPT_CONNECTABLE;
-        //+ BT_LE_ADV_OPT_DIR_MODE_LOW_DUTY;
+        + BT_LE_ADV_OPT_DIR_MODE_LOW_DUTY;  
 
     struct bt_le_adv_param params = {
         .id = 0x0,
         .options = ext_adv_aptions,
         .peer = &receiver_addr,
-        .interval_min = BT_GAP_ADV_FAST_INT_MIN_1, //TODO: advertising interval bigger?
+        .interval_min = BT_GAP_ADV_FAST_INT_MIN_1, 
         .interval_max = BT_GAP_ADV_FAST_INT_MAX_1 
     };
 
@@ -84,19 +70,10 @@ void main(void)
         return;
     }
     
-    // get time in ns as 64b int 
-    /*
-    end_time = timing_counter_get(); 
-    cycles = timing_cycles_get(&start_time, &end_time);
-    time_in_ns = timing_cycles_to_ns(cycles); 
-    */
-    //printk("Measured start time: %llu, cycles: %llu\n", time_in_ns, cycles); 
-
     // Data setup
     // First byte is ***mesh address*** of final destination 
-    // Giving time stamp to the packet 
     uint8_t mfg_data[9]; 
-    memcpy(&mfg_data[1], mfg_data2, sizeof(mfg_data2));
+    memcpy(&mfg_data[1], mfg_data, sizeof(mfg_data));
     mfg_data[0] = 0x00; 
     const struct bt_data ad[] = {BT_DATA(0xff, mfg_data, 9)
     };
@@ -120,58 +97,4 @@ void main(void)
     }
 
     printk("MOBILE BROADCASTER ADV START\n");
-
-    // wait some time to finish 	
-    k_msleep(100);
-    
-    // stop it
-    /*err = bt_le_adv_stop();
-    if (err) {
-        printk("Advertising failed to stop (err %d)\n", err);
-        return;
-    }*/
-    /* 
-    end_time = timing_counter_get(); 
-    cycles = timing_cycles_get(&start_time, &end_time);
-    time_in_ns = timing_cycles_to_ns(cycles); 
-    //printk("Measured start time: %llu, cycles: %llu \n", time_in_ns, cycles); 
-    
-    uint8_t mfg_data2[9]; 
-    memcpy(&mfg_data2[1], &time_in_ns, sizeof(time_in_ns));
-    mfg_data2[0] = 0x00; 
-    const struct bt_data ad2[] = {BT_DATA(0xff, mfg_data2, 9)
-    };
-    */ 
-    // set new data to adv set 
-    // 
-    /*
-    err = bt_le_ext_adv_set_data(adv, ad2, ARRAY_SIZE(ad2),
-            NULL, 0);
-    if(err){
-        printk("Failed to set data for adv (err %d)\n",
-                err); 
-        return;
-    }
-    */
-    
-    //printk("Second message advertising...\n");
-
-    /*
-    do{
-        err = bt_le_ext_adv_start(adv,
-            //BT_LE_EXT_ADV_START_DEFAULT); 
-            BT_LE_EXT_ADV_START_PARAM(0, 15));
-    }while(err);
-    
-    if(err){
-        printk("Failed to start adv (err %d)\n",
-                err); 
-        return;
-    }
-    */
-	
-	do {
-		k_msleep(100);
-
-	} while (1);
 }
