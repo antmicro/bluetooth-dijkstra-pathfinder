520e7701de7724c9edf20a667ac2e722e262761d
jszukala <jszukala@internships.antmicro.com>
2022-07-08 16:41:10 +0200

    Tweaks to parameters to improve performance

--- renode-commands/move_radio.py
+++ renode-commands/move_radio.py
@@ -16,11 +16,13 @@ def mc_move():
     succ, radio= emulationManager.Instance.CurrentEmulation.TryGetEmulationElementByName('sysbus.radio', machine)
     
     x = radius * math.cos(math.pi * degree_counter / 180.0) 
-    y = radius * math.sin(math.pi * degree_counter / 180.0) 
+    y = radius * math.sin(math.pi * degree_counter / 180.0) + 3.0
+
+    print(machine)
     print("X: ", x, " Y: ", y, " degrees: ", degree_counter)
     wireless.SetPosition(radio, x, y, 0)
-    degree_counter += 10.0
-    if degree_counter == 360.0:
+    degree_counter += 5.0
+    if degree_counter > 360.0:
         degree_counter = 0.0
 
 
