1b2ab9855a761201f69d34ef96838a6e128996a1
jszukala <jszukala@internships.antmicro.com>
2022-07-11 16:44:32 +0200

    Simplify the mb movement to reduce strain on renode

--- renode-commands/move_radio.py
+++ renode-commands/move_radio.py
@@ -1,28 +1,28 @@
 import math
 
-x = 0 
-y = 0
-degree_counter = 0.0
+counter = 0
 
 def mc_move():
-    global x
-    global y
-    global degree_counter
-    radius = 5
+    global counter
     
+    mobile_broadcaster_positions = [
+            (-1.5, 1.5, 0.0),
+            (-1.5, 4.5, 0.0),
+            (1.5, 4.5, 0.0),
+            (1.5, 1.5, 0.0)
+            ]
+
     machine = monitor.Machine
     succ, wireless = emulationManager.Instance.CurrentEmulation.TryGetEmulationElementByName('wireless', machine)
 
     succ, radio= emulationManager.Instance.CurrentEmulation.TryGetEmulationElementByName('sysbus.radio', machine)
     
-    x = radius * math.cos(math.pi * degree_counter / 180.0) 
-    y = radius * math.sin(math.pi * degree_counter / 180.0) + 3.0
+    x, y, z = mobile_broadcaster_positions[counter]
 
     print(machine)
-    print("X: ", x, " Y: ", y, " degrees: ", degree_counter)
-    wireless.SetPosition(radio, x, y, 0)
-    degree_counter += 5.0
-    if degree_counter > 360.0:
-        degree_counter = 0.0
+    print("X: ", x, " Y: ", y, "Counter: ", counter)
+    wireless.SetPosition(radio, x, y, z)
+    counter += 1
+    counter = counter % len(mobile_broadcaster_positions) 
 
 
