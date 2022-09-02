d41b5d6bad7ef301fcd22d8087a2f458cb892086
jszukala <jszukala@internships.antmicro.com>
2022-09-02 14:02:24 +0200

    Add asserts build option and use getopts

--- build_as.sh
+++ build_as.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # fail the entire script if any of commands fails or if unset variable will be expanded
-set -eu
+set -e
+set -u
 
 usage () {
     echo "Usage: ./build_as.sh [BUILD CONFIGURATION] [OPTIONS]
@@ -11,18 +12,21 @@ Build the application in one of supported configurations. Build process involves
 * Compiling resultant code into an application
 Generation of graph data structure and in general mesh topology is based on the configuration files in config-files/mesh-topology-desc/*.json.
     Build configurations:
-    --basic        - build basic 5 nodes configuration - config-files/mesh-topology-desc/basic_5_nodes.json
-    --random       - build random topology - config-files/mesh-topology-desc/randomized_topology.json
-    --input        - build from .json file with given name and located at config-files/mesh-topology-desc
+    -b        - build basic 5 nodes configuration - config-files/mesh-topology-desc/basic_5_nodes.json
+    -r        - build random topology - config-files/mesh-topology-desc/randomized_topology.json
+    -i        - build from provided .json file
 
     Options:
-    --randomize    - reshuffles network for given number of nodes, only available with --random
+    -s        - shuffles network for given number of nodes, only available with -r
+    -a        - enable or disable assertions in Zephyr build, available values: 0, 1, 2 with default 1
 
     Example usage:
-    $ ./build_as.sh --basic                   # Build basic 5 nodes static configuration
-    $ ./build_as.sh --random                  # Build random configuration
-    $ ./build_as.sh --random --randomize 10   # Generate new configuration of the network for 10 nodes and build
-    $ ./build_as.sh --input example.json      # Build from user provided config file"
+    $ ./build_as.sh -b                   # Build basic 5 nodes static configuration
+    $ ./build_as.sh -r                   # Build random configuration
+    $ ./build_as.sh -r -s 10             # Generate new configuration of the network for 10 nodes and build
+    $ ./build_as.sh -i example.json      # Build from user provided config file
+    $ ./build_as.sh -b -a 1              # Build with asserts
+    "
 }
 
 # At least configuration argument must be provided
@@ -31,71 +35,80 @@ if [ $# -lt 1 ]; then
     exit 0
 fi
 
-CONFIGURATION=$1
-
-# case statement for picking configuration to run
-case $CONFIGURATION in
-  --random)
-    TOPOLOGY_PATH="config-files/mesh-topology-desc/randomized_topology.json"
-    if [ $# -eq 3 ]; then
-        if [ "$2" == "--randomize" ]; then
+RANDOMIZE=0
+ASSERT_LEVEL=1
+NODES_NUMBER=0
+CONFIGURATION= # May have values b, r, i
+while getopts "bri:s:a:" opt
+do
+    case $opt in
+        # Build configurations
+        b)
+            # If variable is already set
+            if [ -v TOPOLOGY_PATH ]; then
+                usage
+                echo $"ERROR: Provide only one build configuration out of -b -r -i."
+                exit 1
+            fi
+            TOPOLOGY_PATH="config-files/mesh-topology-desc/randomized_topology.json"
+            CONFIGURATION=b
+            ;;
+        r)
+            if [ -v TOPOLOGY_PATH ]; then
+                usage
+                echo $"ERROR: Provide only one build configuration out of -b -r -i."
+                exit 1
+            fi
+            TOPOLOGY_PATH="config-files/mesh-topology-desc/randomized_topology.json"
+            CONFIGURATION=r
+            ;;
+        i)
+            if [ -v TOPOLOGY_PATH ]; then
+                usage
+                echo $"ERROR: Provide only one build configuration out of -b -r -i."
+                exit 1
+            fi
+            TOPOLOGY_PATH=$OPTARG
+            CONFIGURATION=i
+            ;;
+        # Options
+        s)
             RANDOMIZE=1
-            NODES_NUMBER=$3
-        else
+            NODES_NUMBER=$OPTARG
+            ;;
+        a)
+            ASSERT_LEVEL=$OPTARG
+            ;;
+        *)
             usage
-            echo $"ERROR: Unrecognized argument: $2. Did You mean --randomize?"
+            echo $"ERROR: Unrecognized option."
             exit 1
-        fi
-    else
-        RANDOMIZE=0
-    fi
-    if [ $# -eq 2 ]; then
-        usage
-        echo "ERROR: Invalid syntax for --random build."
-        exit 1
-    fi
-    ;;
+            ;;
+    esac
+done
 
-  --basic)
-    TOPOLOGY_PATH="config-files/mesh-topology-desc/basic_5_nodes.json"
-    RANDOMIZE=0
-    ;;
-
-  --input)
-    if [ $# -eq 2 ]; then
-        TOPOLOGY_PATH=config-files/mesh-topology-desc/${2}
-        if [ ! -f "$TOPOLOGY_PATH" ]; then
-            usage
-            echo $"ERROR: provided topology file: ${TOPOLOGY_PATH} does not exist."
-            exit 1
-        fi
-        RANDOMIZE=0
-    else
-        usage
-        echo $"ERROR: Provide name of the topology file."
-        exit 1
-    fi
-    ;;
-
-  *)
+if [ ! -v TOPOLOGY_PATH ]; then
     usage
-    echo "ERROR: Provide correct arguments."
+    echo $"ERROR: invalid configuration options."
     exit 1
-    ;;
-esac
-
-# Randomize / reshuffle number of nodes specified by user and generate 
-# corresponding .json topology and .resc file
-if [ $RANDOMIZE -eq 1 ]; then
-    python3 scripts/topology_randomizer.py $NODES_NUMBER \
-    --mbmove \
-    --verbose \
-    --visualize \
-    --visualizemb \
-    --faulty_nodes 1
 fi
 
-# Generate graph and API 
+if [ $RANDOMIZE -eq 1 ]; then
+    if [ "$CONFIGURATION" == "r" ]; then
+        python3 scripts/topology_randomizer.py $NODES_NUMBER \
+            --mbmove \
+            --verbose \
+            --visualize \
+            --visualizemb \
+            --faulty_nodes 1
+    else
+        usage
+        echo $"ERROR: Randomizing of nodes can only be performed with random build (option -r)"
+        exit 1
+    fi
+fi
+
+# Generate graph and API
 python3 scripts/generator.py $TOPOLOGY_PATH
 
 # Build network
@@ -103,11 +116,14 @@ west build -b nrf52840dk_nrf52840 \
         node/ \
         -d node/build \
         -- -DMAX_TTL=3 \
+        -D__ASSERT_ON=$ASSERT_LEVEL
 
 # Build mobile_broadcaster
 west build -b nrf52840dk_nrf52840 \
         mobile_broadcaster \
-        -d mobile_broadcaster/build
+        -d mobile_broadcaster/build \
+        -D__ASSERT_ON=$ASSERT_LEVEL
+
 
 
 
